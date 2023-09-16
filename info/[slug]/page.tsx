bf46351082c13ae16ed589a1603bb356b77f3014
Gustavo <gustavo.vinhola27@gmail.com>
2023-09-15 23:03:59 -0300

    Mais alteraçoes

--- info/[slug]/page.tsx
+++ info/[slug]/page.tsx
@@ -1,29 +1,11 @@
-import exp from "constants";
-import Image from "next/image";
 export async function generateStaticParams() {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon');
     const { results: pokemons } = await response.json();
     return pokemons.map((pokemon: any) => ({ slug: pokemon.name }));
 }
-
 export default async function InfoDetails({params, }: { params: { slug: string};}) {
     const response = await fetch(`https://pokeapi.com/api/v2/pokemon/${params.slug}`);
     const pokemon = await response.json();
 
     return <div><h2>{pokemon.name}</h2></div>;
-}
-
-    
-function Pokemon() {
-    return (
-       <div className= "flex items-center">
-        <Image
-            src={Pokemon.sprites.front_shiny}
-            width={80}
-            height={80}
-            alt={Pokemon.name}
-        />
-        <h2 className="text-2xl mb-4 font-bold">{Pokemon.name}</h2>
-       </div> 
-    );
 }
